5b12e6dc3ce0cb3f6c4c576fd0beb77cf46f9cd7
Amr Eladly <amreladly09@gmail.com>
2025-01-03 00:00:22 +0100

    11

--- dieRoll.java
+++ dieRoll.java
@@ -1,32 +1,30 @@
 import java.util.Scanner;
 
 public class dieRoll {
-    public static void gcd() {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        scanner.close();
-        int max = (x > y) ? x : y;
-        // formula for finding the gcd using Euclidean algorithm
-        // A = x B + remainder1 --> B = y C + remainder2, till remainder = 0
-        int probability = 6 + 1 - max;
-        if (probability == 6) {
-            System.out.println("1/1");
-        } else {
-            int remainder = 6 - probability;
-            int b = probability;
-            int A;
-            while (true) {
-                if(remainder == 0){ break;}
-                A = b;
-                b = remainder;
-                remainder = A - ((int)A/b)*b;
-            }
-            System.out.println(probability/b +"/"+ 6/b);
-        }
-    }
 
-    public static void main(String[] args){
-        gcd();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int max = (x > y) ? x : y;
+            int probability = 7 - max;
+            
+            if (probability <= 0) {
+                System.out.println("0/1");
+            } else {
+                int remainder, a, b;
+                a = probability;
+                b = 6;
+                while (b != 0) {
+                    remainder = a % b;
+                    a = b;
+                    b = remainder;
+                }
+                System.out.println((probability / a) + "/" + (6 / a));
+            }
+        } finally {
+            scanner.close();
+        }
     }
 }
