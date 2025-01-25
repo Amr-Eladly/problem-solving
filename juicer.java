052b33fa6ddefd53a8d7652a5fb0d2410fa9a0ac
Amr Eladly <amreladly09@gmail.com>
2025-01-25 03:35:19 +0100

    13

--- juicer.java
+++ juicer.java
@@ -1,5 +1,3 @@
-import java.nio.channels.Pipe.SourceChannel;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class juicer {
@@ -9,24 +7,26 @@ public class juicer {
             int orangesNumber = scanner.nextInt();
             int maxSize = scanner.nextInt();
             int wasteSectionCapacity = scanner.nextInt();
-            // ArrayList<Integer> oragnes = new ArrayList<>();
             int sum = 0;
             int counter = 0;
+
             for (int index = 0; index < orangesNumber; index++) {
+
                 int x = scanner.nextInt();
                 if (x <= maxSize) {
                     sum += x;
+
                     if (sum > wasteSectionCapacity) {
                         counter += 1;
                         sum = 0;
                     }
-                    // oragnes.add(scanner.nextInt());
                 } else {
                     continue;
                 }
             }
 
             System.out.println(counter);
+
         } finally {
             scanner.close();
         }
